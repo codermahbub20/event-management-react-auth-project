bbbc7351f55bfb55adbd09dea15cda2f44deb901
codermahbub20 <mkrefat5@gmail.com>
2023-10-08 00:31:01 +0600

    Footer Section are created

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -1,40 +1,33 @@
 
-
+// style={{ backgroundImage: 'url(https://www.rajibweb.com/html/fitme/images/bg/footer.jpg)' }}
 const Footer = () => {
     return (
-        <div className="hero min-h-[60vh]" style={{ backgroundImage: 'url(https://www.rajibweb.com/html/fitme/images/bg/footer.jpg)' }}>
-            <div className="hero-overlay bg-black bg-opacity-90"></div>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-full">
-                    <footer className="footer  w-full p-10 bg-base-200 text-base-content">
+        <footer  className="footer w-3/4  mx-auto p-10  text-base-content">
                         <aside>
                             <img className="w-44" src="https://helthify-demo.pbminfotech.com/demo2/wp-content/uploads/sites/3/2022/06/logo-1.png" alt="" />
-                            <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                            <p className="text-xl">Healthify Industries Ltd.<br />Providing reliable tech since 1992</p>
                         </aside>
-                        <nav>
+                        <nav className="text-xl">
                             <header className="footer-title">Services</header>
                             <a className="link link-hover">Branding</a>
                             <a className="link link-hover">Design</a>
                             <a className="link link-hover">Marketing</a>
                             <a className="link link-hover">Advertisement</a>
                         </nav>
-                        <nav>
+                        <nav className="text-xl">
                             <header className="footer-title">Company</header>
                             <a className="link link-hover">About us</a>
                             <a className="link link-hover">Contact</a>
                             <a className="link link-hover">Jobs</a>
                             <a className="link link-hover">Press kit</a>
                         </nav>
-                        <nav>
+                        <nav className="text-xl">
                             <header className="footer-title">Legal</header>
                             <a className="link link-hover">Terms of use</a>
                             <a className="link link-hover">Privacy policy</a>
                             <a className="link link-hover">Cookie policy</a>
                         </nav>
                     </footer>
-                </div>
-            </div>
-        </div>
     );
 };
 
